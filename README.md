3476b4c293dc2b00cf8391b6392b7c1a4265bea1
Tamas Kalman <tamas@mobileplusinc.com>
2012-11-26 11:10:17 -0800

    fireBase PHP Client skeleton

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ firebase-php
 ============
 
 Firebase PHP Client
+
+Based on Firebase REST API: https://www.firebase.com/docs/rest-api.html
---
+++ firebaseLib.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Firebase PHP Client Library
+ *
+ * @author Tamas Kalman <ktamas77@gmail.com>
+ * @link   https://www.firebase.com/docs/rest-api.html
+ * 
+ */
+
+/**
+ * Firebase PHP Class
+ *
+ * @author Tamas Kalman <ktamas77@gmail.com>
+ * @link   https://www.firebase.com/docs/rest-api.html
+ *
+ */
+class Firebase {
+
+    /**
+     * Constructor
+     */
+    function __construct() {
+
+    }
+
+    /**
+     * Writing data into Firebase with a PUT request
+     * 
+     * @param type $obj
+     */
+    public function write($obj)
+    {
+        
+    }
+
+    /**
+     * Reading data from Firebase
+     *
+     * @param type $obj
+     */
+    public function read($obj)
+    {
+
+    }
+
+    /**
+     * Pushing data to Firebase
+     *
+     * @param type $obj
+     */
+    public function push($obj)
+    {
+
+    }
+
+    public function delete($endPoint)
+    {
+        
+    }
+
+}
